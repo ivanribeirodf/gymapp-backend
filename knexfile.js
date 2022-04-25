ef286a3fc9e81a5093ef761aaaaa0add5b05f6c8
IvanPix <ivan@pix.com.br>
2022-04-25 15:05:46 -0300

    Gym APP inicial

---
+++ knexfile.js
@@ -0,0 +1,40 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: 'localhost',
+      database: 'gymapp',
+      user: 'postgres',
+      password: 'pix2000'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './src/database/migrations'
+    },
+    useNullAsDefault: true
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+}
